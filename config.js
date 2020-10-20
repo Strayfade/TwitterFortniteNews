19a3f7c5c6d013f1b9aecf1b0821850e514de8f6
Strayfade <64668046+Strayfade@users.noreply.github.com>
2020-10-20 12:58:46 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-
-// Twitter App Configuration (twit module)
-
-module.exports = {
-    consumer_key: '',         // API KEY
-    consumer_secret: '',      // API SECRET
-    access_token: '',         // ACCESS TOKEN
-    access_token_secret: ''   // ACCESS SECRET
-  }
